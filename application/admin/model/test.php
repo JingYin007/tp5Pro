bf082a895c73132710b442cb4b5d6d02d0afd728
moTzxx <JingYin007@users.noreply.github.com>
2017-08-21 11:35:02 +0800

    build.php
    自动生成 模块

---
+++ application/admin/model/test.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\admin\model;
+
+use think\Model;
+
+class test extends Model
+{
+
+}
--- application/build.php
+++ application/build.php
@@ -14,12 +14,16 @@ return [
     '__file__' => ['common.php', 'config.php', 'database.php'],
 
     // 定义demo模块的自动生成 （按照实际定义的文件名生成）
-    'wos'     => [
-        '__file__'   => ['common.php'],
-        '__dir__'    => ['behavior', 'controller', 'model', 'view'],
-        'controller' => ['Index', 'Test', 'UserType'],
-        'model'      => ['User', 'UserType'],
-        'view'       => ['index/index'],
-    ],
+
     // 其他更多的模块定义
+    'common' => [
+        '__dir__' => ['model','view','controller'],
+        'model' => ['index'],
+    ],
+    'admin' => [
+        '__dir__' => ['model','view','controller'],
+        'model' => ['test'],
+        'view' => ['index/index','order/index','order/test'],
+    ],
+    
 ];
